699e9779461471bf7ba0979b024b962e9a105c68
ddiax09 <hdiaz3570@gmail.com>
2024-12-09 19:57:21 -0600

    Minor errors solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 
 import { AnimatedTestimonialsDemo } from "./components/about-me-section";
-import { Footer } from "./components/footer";
 import { HeroSection } from "./components/hero-section";
 import { MyWork } from "./components/my-work";
-import { NavBar } from "./components/navbar";
 
 export default function Home() {
   return (
